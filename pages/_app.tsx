be444935e4e7531fd73b0dcc53973fc1b75d2c31
rezonmain <adiazgomez101@gmail.com>
2022-08-10 16:17:25 -0700

    added pageloading component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,13 @@ import type { AppProps } from 'next/app';
 import { ChakraProvider } from '@chakra-ui/react';
 import theme from '../themes/chakraTheme';
 import { useRouter } from 'next/router';
-
+import NextNProgress from 'nextjs-progressbar';
 function MyApp({ Component, pageProps }: AppProps) {
 	const router = useRouter();
 	return (
 		<ChakraProvider theme={theme}>
 			<Component key={router.asPath} {...pageProps} />
+			<NextNProgress height={5} startPosition={0} />
 		</ChakraProvider>
 	);
 }
